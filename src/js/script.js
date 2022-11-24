9bff778510ae58a0fcc63b6d8316119ee73ac993
Maxim <maxdemented372@gmail.com>
2022-11-24 18:54:49 +0600

    qazapartReady

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,11 @@
-$(document).ready(function(){
-
-
-
-    $('.catalog__button').on('click', '.catalog__button:not(.active)', function() {
-        $(this)
-        .addClass('active').siblings().removeClass('active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-    });
+const hamburger = document.querySelector(".hamburger"),
+      navbar = document.querySelector(".navbar__wrapper"),
+      cross = document.querySelector(".hamburger.close");
 
 
 
 
+hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    hamburger.classList.toggle("close")
 });
